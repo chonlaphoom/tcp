e0246df5c741a54447b2e61fe8a4addc346db6a6
chonlaphoom <chonlaphoomth@gmail.com>
2025-09-07 18:56:03 +0700

    refact

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 func main() {
-	file, _ := os.Open("messages.txt")
+	const inputFilePath = "messages.txt"
+
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
 	defer file.Close()
 
-	readBuffer := make([]byte, 8)
+	readBuffer := make([]byte, 8, 8)
 	for {
 		_, errorRead := file.Read(readBuffer)
 		if errorRead != nil {
+			if errors.Is(errorRead, io.EOF) {
+				break
+			}
+			fmt.Printf("could not read from %s: %s\n", inputFilePath, errorRead)
 			break
 		}
 
