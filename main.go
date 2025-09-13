8a907557cc742f36c59475be6d26ef1cc2731be8
chonlaphoom <chonlaphoomth@gmail.com>
2025-09-13 13:41:22 +0700

    move logic to go routine

--- main.go
+++ main.go
@@ -16,31 +16,48 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer file.Close()
 
-	var currentLine string
-	for {
-		readBuffer := make([]byte, 8, 8)
-		upto, errorRead := file.Read(readBuffer)
-		if errorRead != nil {
-			if errors.Is(errorRead, io.EOF) {
-				if currentLine != "" {
-					fmt.Print("read: ", currentLine)
-					currentLine = ""
-				}
-				break
-			}
-			fmt.Printf("could not read from %s: %s\n", inputFilePath, errorRead)
-			break
-		}
+	c := getLinesChannel(file)
 
-		parts := strings.Split(string(readBuffer[:upto]), "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLine, parts[i])
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
+	for line := range c {
+		fmt.Print(line)
 	}
 
 	os.Exit(0)
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		defer file.Close()
+		defer close(c)
+
+		currentLine := ""
+		for {
+			readBuffer := make([]byte, 8)
+			upto, errorRead := file.Read(readBuffer)
+			if errorRead != nil {
+				if errors.Is(errorRead, io.EOF) {
+					if currentLine != "" {
+						fmt.Print("read: ", currentLine)
+						c <- fmt.Sprint("read: ", currentLine)
+					}
+					break
+				}
+
+				fmt.Printf("could not read from file: %s\n", errorRead)
+				break
+			}
+
+			parts := strings.Split(string(readBuffer[:upto]), "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				c <- fmt.Sprintf("read: %s%s\n", currentLine, parts[i])
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+
+	return c
 }
